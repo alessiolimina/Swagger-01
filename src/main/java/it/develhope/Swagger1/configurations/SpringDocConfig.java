edadda014d2b94bbca228473bbddac87d582ee32
Alessio Limina <alessio.limina@gmail.com>
2023-04-17 21:55:56 +0200

    Exercise Swagger 01

---
+++ src/main/java/it/develhope/Swagger1/configurations/SpringDocConfig.java
@@ -0,0 +1,16 @@
+package it.develhope.Swagger1.configurations;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition
+@Configuration
+public class SpringDocConfig {
+@Bean
+    public OpenAPI baseOpenApi(){
+    return new OpenAPI().info(new Info().title("Spring Doc").version("1.0.0").description("Swagger example"));
+    }
+}
