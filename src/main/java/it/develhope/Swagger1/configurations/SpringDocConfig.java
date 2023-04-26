d5011af5ec2ad3eb4641370028152c7bc01f1c61
Alessio Limina <alessio.limina@gmail.com>
2023-04-26 19:56:09 +0200

    Exercise Swagger 01 - added fixme

--- src/main/java/it/develhope/Swagger1/configurations/SpringDocConfig.java
+++ src/main/java/it/develhope/Swagger1/configurations/SpringDocConfig.java
@@ -2,16 +2,27 @@ package it.develhope.Swagger1.configurations;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.yaml.snakeyaml.nodes.Tag;
+
+import java.util.List;
 
 @OpenAPIDefinition
 @Configuration
 public class SpringDocConfig {
 @Bean
     public OpenAPI baseOpenApi(){
-    return new OpenAPI().info(new Info().title("Spring Doc").version("1.0.0").description("Swagger example"));
+    return new OpenAPI().info(new Info().title("Exercise Swagger-01")
+                    .version("1.0.0")
+                    .description("An exercise illustrating Swagger")
+                    .contact(new Contact()
+                            .name("Alessio Limina")
+                            .email("alessio.limina@gmail.com")
+                            .url("https://www.develhope.co")))
+                    .tags(List.of())
     }
 }
 //fixme how to insert method tags in this config file?
